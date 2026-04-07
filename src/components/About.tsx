8c7fc747419a68cc5fd0ab8fc35a5f9d12ce708c
SohailAsgharWorkOfficial <sohailasghar.workofficial@gmail.com>
2026-04-07 18:08:31 +0500

    push

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,9 +7,9 @@ const About = () => {
         <h3 className="title">About Me</h3>
         <p className="para">
           Full Stack Developer with over 3 years of experience designing, developing, and maintaining modern, scalable web applications. Proficient
-in React.js, Next.js, Node.js, TypeScript, and database management with MongoDB and MySQL. Skilled in building responsive, highperformance frontends and robust backends while collaborating with cross-functional teams to deliver production ready solutions.
-Experienced in REST API integration, cloud deployment, and optimizing user experiences. Passionate about maintainable code,
-performance optimization, and utilizing technology to create efficient, user-friendly applications and leading small development initiatives.
+          in React.js, Next.js, Node.js, TypeScript, and database management with MongoDB and MySQL. Skilled in building responsive, highperformance frontends and robust backends while collaborating with cross-functional teams to deliver production ready solutions.
+          Experienced in REST API integration, cloud deployment, and optimizing user experiences. Passionate about maintainable code,
+          performance optimization, and utilizing technology to create efficient, user-friendly applications and leading small development initiatives.
         </p>
       </div>
     </div>
